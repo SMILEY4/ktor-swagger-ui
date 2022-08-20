88fd736f223c4f1c4a0dfb58a25e4871177c8ef5
Lukas Ruegner <ruegnerlukas@gmail.com>
2022-08-21 00:07:00 +0200

    split basic example

--- src/test/kotlin/io/github/smiley4/ktorswaggerui/examples/BasicExample.kt
+++ src/test/kotlin/io/github/smiley4/ktorswaggerui/examples/BasicExample.kt
@@ -1,23 +1,14 @@
 package io.github.smiley4.ktorswaggerui.examples
 
-import com.fasterxml.jackson.core.util.DefaultIndenter
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
-import com.fasterxml.jackson.databind.SerializationFeature
 import io.github.smiley4.ktorswaggerui.SwaggerUI
 import io.github.smiley4.ktorswaggerui.documentation.get
-import io.github.smiley4.ktorswaggerui.documentation.post
 import io.ktor.http.HttpStatusCode
-import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.request.receive
-import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.routing
-import java.util.Random
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "localhost") {
@@ -34,16 +25,6 @@ fun main() {
             server {
                 url = "http://localhost:8080"
                 description = "Development Server"
-            }
-            automaticTagGenerator = { url -> url.firstOrNull() }
-        }
-        install(ContentNegotiation) {
-            jackson {
-                configure(SerializationFeature.INDENT_OUTPUT, true)
-                setDefaultPrettyPrinter(DefaultPrettyPrinter().apply {
-                    indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
-                    indentObjectsWith(DefaultIndenter("  ", "\n"))
-                })
             }
         }
         routing {
@@ -62,79 +43,6 @@ fun main() {
             }) {
                 call.respondText("Hello World!")
             }
-            post("math/{operation}", {
-                tags = listOf("test")
-                description = "Performs the given operation on the given values and returns the result"
-                request {
-                    pathParameter("operation", String::class.java) {
-                        description = "the math operation to perform. Either 'add' or 'sub'"
-                    }
-                    body(MathRequest::class.java)
-                }
-                response {
-                    HttpStatusCode.OK to {
-                        description = "The operation was successful"
-                        body(MathResult::class.java) {
-                            description = "The result of the operation"
-                        }
-                    }
-                    HttpStatusCode.BadRequest to {
-                        description = "An invalid operation was provided"
-                    }
-                }
-            }) {
-                val operation = call.parameters["operation"]!!
-                call.receive<MathRequest>().let { request ->
-                    when (operation) {
-                        "add" -> call.respond(HttpStatusCode.OK, MathResult(request.a + request.b))
-                        "sub" -> call.respond(HttpStatusCode.OK, MathResult(request.a - request.b))
-                        else -> call.respond(HttpStatusCode.BadRequest, Unit)
-                    }
-                }
-            }
-            post("random/results", {
-                response {
-                    HttpStatusCode.OK to {
-                        body(Array<MathResult>::class.java)
-                    }
-                }
-            }) {
-                call.respond(HttpStatusCode.OK, (0..5).map { MathResult(Random().nextInt()) })
-            }
-            post("random/numbers", {
-                response {
-                    HttpStatusCode.OK to {
-                        body(IntArray::class.java)
-                    }
-                }
-            }) {
-                call.respond(HttpStatusCode.OK, (0..5).map { Random().nextInt() })
-            }
-            post("echo/{color}", {
-                request {
-                    pathParameter("color", Color::class.java)
-                }
-                response {
-                    HttpStatusCode.OK to {
-                        body(String::class.java)
-                    }
-                }
-            }) {
-                call.respond(HttpStatusCode.OK, Color.valueOf(call.parameters["color"]!!).toString())
-            }
         }
     }.start(wait = true)
-}
-
-data class MathRequest(
-    val a: Int,
-    val b: Int
-)
-
-data class MathResult(
-    val value: Int
-)
-
-enum class Color {
-    RED, GREEN, BLUE
 }
---
+++ src/test/kotlin/io/github/smiley4/ktorswaggerui/examples/CompleteExample.kt
@@ -0,0 +1,140 @@
+package io.github.smiley4.ktorswaggerui.examples
+
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.github.smiley4.ktorswaggerui.SwaggerUI
+import io.github.smiley4.ktorswaggerui.documentation.get
+import io.github.smiley4.ktorswaggerui.documentation.post
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.jackson.jackson
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.routing
+import java.util.Random
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "localhost") {
+        install(SwaggerUI) {
+            swagger {
+                swaggerUrl = "swagger-ui"
+                forwardRoot = true
+            }
+            info {
+                title = "Example API"
+                version = "latest"
+                description = "Example API for testing and demonstration purposes."
+            }
+            server {
+                url = "http://localhost:8080"
+                description = "Development Server"
+            }
+            automaticTagGenerator = { url -> url.firstOrNull() }
+        }
+        install(ContentNegotiation) {
+            jackson {
+                configure(SerializationFeature.INDENT_OUTPUT, true)
+                setDefaultPrettyPrinter(DefaultPrettyPrinter().apply {
+                    indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+                    indentObjectsWith(DefaultIndenter("  ", "\n"))
+                })
+            }
+        }
+        routing {
+            get("hello", {
+                tags = listOf("test")
+                description = "Hello World Endpoint"
+                response {
+                    HttpStatusCode.OK to {
+                        description = "Successful Request"
+                        body(String::class.java) { description = "the response" }
+                    }
+                    HttpStatusCode.InternalServerError to {
+                        description = "Something unexpected happened"
+                    }
+                }
+            }) {
+                call.respondText("Hello World!")
+            }
+            post("math/{operation}", {
+                tags = listOf("test")
+                description = "Performs the given operation on the given values and returns the result"
+                request {
+                    pathParameter("operation", String::class.java) {
+                        description = "the math operation to perform. Either 'add' or 'sub'"
+                    }
+                    body(MathRequest::class.java)
+                }
+                response {
+                    HttpStatusCode.OK to {
+                        description = "The operation was successful"
+                        body(MathResult::class.java) {
+                            description = "The result of the operation"
+                        }
+                    }
+                    HttpStatusCode.BadRequest to {
+                        description = "An invalid operation was provided"
+                    }
+                }
+            }) {
+                val operation = call.parameters["operation"]!!
+                call.receive<MathRequest>().let { request ->
+                    when (operation) {
+                        "add" -> call.respond(HttpStatusCode.OK, MathResult(request.a + request.b))
+                        "sub" -> call.respond(HttpStatusCode.OK, MathResult(request.a - request.b))
+                        else -> call.respond(HttpStatusCode.BadRequest, Unit)
+                    }
+                }
+            }
+            post("random/results", {
+                response {
+                    HttpStatusCode.OK to {
+                        body(Array<MathResult>::class.java)
+                    }
+                }
+            }) {
+                call.respond(HttpStatusCode.OK, (0..5).map { MathResult(Random().nextInt()) })
+            }
+            post("random/numbers", {
+                response {
+                    HttpStatusCode.OK to {
+                        body(IntArray::class.java)
+                    }
+                }
+            }) {
+                call.respond(HttpStatusCode.OK, (0..5).map { Random().nextInt() })
+            }
+            post("echo/{color}", {
+                request {
+                    pathParameter("color", Color::class.java)
+                }
+                response {
+                    HttpStatusCode.OK to {
+                        body(String::class.java)
+                    }
+                }
+            }) {
+                call.respond(HttpStatusCode.OK, Color.valueOf(call.parameters["color"]!!).toString())
+            }
+        }
+    }.start(wait = true)
+}
+
+data class MathRequest(
+    val a: Int,
+    val b: Int
+)
+
+data class MathResult(
+    val value: Int
+)
+
+enum class Color {
+    RED, GREEN, BLUE
+}
