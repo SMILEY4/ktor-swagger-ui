074e8dff1a2dd763982c75fc52f4b945c620f825
Lukas Ruegner <ruegnerlukas@gmail.com>
2023-05-26 20:25:27 +0200

    print exceptions during api-generation

--- src/main/kotlin/io/github/smiley4/ktorswaggerui/SwaggerPlugin.kt
+++ src/main/kotlin/io/github/smiley4/ktorswaggerui/SwaggerPlugin.kt
@@ -1,16 +1,42 @@
 package io.github.smiley4.ktorswaggerui
 
-import io.github.smiley4.ktorswaggerui.spec.openapi.*
+import io.github.smiley4.ktorswaggerui.spec.openapi.ComponentsBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ContactBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ContentBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ExampleBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ExternalDocumentationBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.HeaderBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.InfoBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.LicenseBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.OAuthFlowsBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.OpenApiBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.OperationBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.OperationTagsBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ParameterBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.PathBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.PathsBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.RequestBodyBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ResponseBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ResponsesBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.SecurityRequirementsBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.SecuritySchemesBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.ServerBuilder
+import io.github.smiley4.ktorswaggerui.spec.openapi.TagBuilder
 import io.github.smiley4.ktorswaggerui.spec.route.RouteCollector
 import io.github.smiley4.ktorswaggerui.spec.route.RouteDocumentationMerger
 import io.github.smiley4.ktorswaggerui.spec.route.RouteMeta
 import io.github.smiley4.ktorswaggerui.spec.schema.SchemaBuilder
 import io.github.smiley4.ktorswaggerui.spec.schema.SchemaContext
 import io.github.smiley4.ktorswaggerui.spec.schema.SchemaContextBuilder
-import io.ktor.server.application.*
-import io.ktor.server.application.hooks.*
-import io.ktor.server.routing.*
-import io.ktor.server.webjars.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStarted
+import io.ktor.server.application.createApplicationPlugin
+import io.ktor.server.application.hooks.MonitoringEvent
+import io.ktor.server.application.install
+import io.ktor.server.application.plugin
+import io.ktor.server.application.pluginOrNull
+import io.ktor.server.routing.Routing
+import io.ktor.server.webjars.Webjars
 import io.swagger.v3.core.util.Json
 
 /**
@@ -19,24 +45,24 @@ import io.swagger.v3.core.util.Json
 internal const val SWAGGER_UI_WEBJARS_VERSION = "4.15.0"
 
 val SwaggerUI = createApplicationPlugin(name = "SwaggerUI", createConfiguration = ::SwaggerUIPluginConfig) {
-
     var apiSpecJson = "{}"
-
     on(MonitoringEvent(ApplicationStarted)) { application ->
         if (application.pluginOrNull(Webjars) == null) {
             application.install(Webjars)
         }
         val routes = routes(application, pluginConfig)
         val schemaContext = schemaContext(pluginConfig, routes)
-        apiSpecJson = Json.pretty(builder(pluginConfig, schemaContext).build(routes))
+        try {
+            apiSpecJson = Json.pretty(builder(pluginConfig, schemaContext).build(routes))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
-
     SwaggerRouting(
         pluginConfig.getSwaggerUI(),
         application.environment.config,
         SWAGGER_UI_WEBJARS_VERSION,
     ) { apiSpecJson }.setup(application)
-
 }
 
 private fun routes(application: Application, pluginConfig: SwaggerUIPluginConfig): List<RouteMeta> {
