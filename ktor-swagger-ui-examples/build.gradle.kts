ec02a752737d12faf8ad4fc9bd66b4e860c821ae
Sahit Tuntas Sadono <sahitsadono@gmail.com>
2024-10-16 20:27:09 +0700

    chore: bump kotlinx serialization

--- ktor-swagger-ui-examples/build.gradle.kts
+++ ktor-swagger-ui-examples/build.gradle.kts
@@ -5,7 +5,7 @@ version = projectVersion
 
 plugins {
     kotlin("jvm")
-    kotlin("plugin.serialization") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 repositories {
