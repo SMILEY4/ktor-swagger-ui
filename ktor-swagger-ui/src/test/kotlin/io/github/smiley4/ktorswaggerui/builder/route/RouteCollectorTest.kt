e5e7c8e90368449ab3f5c1bb968a89b2ed4c22e6
Jens Brimfors <jens.alexander.brimfors@wolt.com>
2024-10-23 06:59:29 +0000

    Transparent route selectors are generating double slashes in the generated swagger

--- ktor-swagger-ui/src/test/kotlin/io/github/smiley4/ktorswaggerui/builder/route/RouteCollectorTest.kt
+++ ktor-swagger-ui/src/test/kotlin/io/github/smiley4/ktorswaggerui/builder/route/RouteCollectorTest.kt
@@ -1,30 +1,36 @@
 package io.github.smiley4.ktorswaggerui.builder.route
 
 import io.github.smiley4.ktorswaggerui.data.PluginConfigData
+import io.kotest.matchers.shouldBe
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCallPipeline
 import io.ktor.server.application.RouteScopedPlugin
-import io.ktor.server.application.call
 import io.ktor.server.application.createRouteScopedPlugin
-import io.ktor.server.application.install
+import io.ktor.server.application.plugin
 import io.ktor.server.response.respond
+import io.ktor.server.routing.HttpMethodRouteSelector
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.RouteSelector
+import io.ktor.server.routing.RouteSelectorEvaluation
+import io.ktor.server.routing.RoutingNode
+import io.ktor.server.routing.RoutingResolveContext
+import io.ktor.server.routing.RoutingRoot
 import io.ktor.server.routing.get
+import io.ktor.server.routing.intercept
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import io.ktor.server.testing.testApplication
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.slf4j.MDC
 import java.util.UUID
 
-data class RouteMeta(
+private data class RouteMetas(
     val method: String,
 )
 
-private val tracer: RouteScopedPlugin<RouteMeta> = createRouteScopedPlugin(
+private val tracer: RouteScopedPlugin<RouteMetas> = createRouteScopedPlugin(
     name = "pluggy",
-    createConfiguration = { RouteMeta("asdf") },
+    createConfiguration = { RouteMetas("asdf") },
     body = {
         onCall {
             if (MDC.get("Trace") == null) {
@@ -36,51 +42,62 @@ private val tracer: RouteScopedPlugin<RouteMeta> = createRouteScopedPlugin(
 
 class RouteCollectorTest {
 
-    private val tests: List<Pair<List<String>, String>> = listOf(
-        listOf("/") to "",
-        listOf("/api") to "/api",
-        listOf("/nested", "/routing") to "/nested/routing",
-        listOf("/trailing/", "/slashes") to "/trailing/slashes",
-    )
+    private fun allRoutes(root: RoutingNode): List<RoutingNode> {
+        return (listOf(root) + root.children.flatMap { allRoutes(it) })
+            .filter { it.selector is HttpMethodRouteSelector }
+    }
 
-    private fun Application.testRoutes(): List<Route> {
-        val memo = mutableListOf<Route>()
+    private fun Application.allRoutesPlease(): Pair<RouteCollector, List<RoutingNode>> {
+        val routeCollector = RouteCollector(RouteDocumentationMerger())
+        val rootNode = plugin(RoutingRoot)
+        val allRoutes = allRoutes(rootNode)
+        return routeCollector to allRoutes
+    }
+
+    private fun Application.rutten() {
         routing {
-            memo.add(get { call.respond("root") })
-            route("/api") {
-                memo.add(get { call.respond("Api") })
-            }
-            route("/nested") {
-                route("/routing") {
-                    install(tracer)
-                    memo.add(get("/") { call.respond("Nested Routing") })
-                }
-            }
-            route("/trailing/") {
-                memo.add(get("/slashes") { call.respond("Trailing Slashes") })
+            install(createRouteScopedPlugin("FooPlugg", { "" },{}))
+            innerRoute()
+        }
+    }
+
+    private fun Route.innerRoute() {
+        route("/api") {
+            auth {
+                get("/v1/get_this") { call.respond("Api") }
             }
         }
-        return memo
+    }
+
+    private fun Route.auth(build: Route.() -> Unit): Route {
+        val guardedRoute = createChild(TransparentRouteSelector())
+        guardedRoute.intercept(ApplicationCallPipeline.Plugins) {
+            proceed()
+        }
+        guardedRoute.build()
+        return guardedRoute
+    }
+
+    private class TransparentRouteSelector : RouteSelector() {
+        override suspend fun evaluate(
+            context: RoutingResolveContext,
+            segmentIndex: Int,
+        ): RouteSelectorEvaluation = RouteSelectorEvaluation.Transparent
+
+        override fun toString(): String = ""
     }
 
     @Test
-    fun `should be able to get nested route`() {
-        // Given
+    fun `should be able to handle transparent route selectors`() {
         testApplication {
             application {
-                val routes = testRoutes()
+                rutten()
+                val (routeCollector, allRoutes) = allRoutesPlease()
 
-                val routeCollector = RouteCollector(RouteDocumentationMerger())
-
-                for (i in tests.indices) {
-                    // When
-                    val nestedRoute = routeCollector.getPath(routes[i], PluginConfigData.DEFAULT)
-
-                    // Then
-                    assertThat(nestedRoute, equalTo(tests[i].second))
-                }
+                val actual = routeCollector.getPath(allRoutes[0], PluginConfigData.DEFAULT)
+                println("actual = $actual")
+                actual shouldBe "/api/v1/get_this"
             }
         }
-
     }
 }
