de20629d79b04b0bd01e057d5531ab88ef8b6f7a
Jens Brimfors <jens.alexander.brimfors@wolt.com>
2024-10-23 10:13:09 +0000

    Making it clear in the test why there is no work-around using the ignoredRouteSelectors

--- ktor-swagger-ui/src/test/kotlin/io/github/smiley4/ktorswaggerui/builder/route/RouteCollectorTest.kt
+++ ktor-swagger-ui/src/test/kotlin/io/github/smiley4/ktorswaggerui/builder/route/RouteCollectorTest.kt
@@ -1,12 +1,16 @@
 package io.github.smiley4.ktorswaggerui.builder.route
 
+import io.github.smiley4.ktorswaggerui.SwaggerUI
+import io.github.smiley4.ktorswaggerui.builder.route.ExternalCode.installTransparentRoute
 import io.github.smiley4.ktorswaggerui.data.PluginConfigData
 import io.github.smiley4.ktorswaggerui.dsl.routing.route
 import io.kotest.matchers.shouldBe
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.application.plugin
 import io.ktor.server.response.respond
 import io.ktor.server.routing.HttpMethodRouteSelector
+import io.ktor.server.routing.Route
 import io.ktor.server.routing.RouteSelector
 import io.ktor.server.routing.RouteSelectorEvaluation
 import io.ktor.server.routing.RoutingNode
@@ -25,38 +29,55 @@ class RouteCollectorTest {
             .filter { it.selector is HttpMethodRouteSelector }
     }
 
-    private fun Application.allRoutesPlease(): Pair<RouteCollector, List<RoutingNode>> {
-        val routeCollector = RouteCollector(RouteDocumentationMerger())
+    private fun Application.allRoutesPlease(): List<RoutingNode> {
         val rootNode = plugin(RoutingRoot)
         val allRoutes = allRoutes(rootNode)
-        return routeCollector to allRoutes
+        return allRoutes
     }
 
-    private class TransparentRouteSelector : RouteSelector() {
-        override suspend fun evaluate(
-            context: RoutingResolveContext,
-            segmentIndex: Int,
-        ): RouteSelectorEvaluation = RouteSelectorEvaluation.Transparent
-
-        override fun toString(): String = ""
+    private fun Application.installSwaggerUI() {
+        install(SwaggerUI) {
+            ignoredRouteSelectors = PluginConfigData.DEFAULT.ignoredRouteSelectors
+            // + setOf(ExternalCode.TransparentRouteSelector::class)
+            // Unable to add the TransparentRouteSelector to the ignoredRouteSelectors
+        }
     }
 
     @Test
     fun `should be able to handle transparent route selectors`() {
+        // Given
         testApplication {
             application {
+                installSwaggerUI()
                 routing {
-                    createChild(TransparentRouteSelector()).route {
+                    installTransparentRoute {
                         get("/v1/get_this") { call.respond("Api") }
                     }
-
                 }
-                val (routeCollector, allRoutes) = allRoutesPlease()
+                val allRoutes = allRoutesPlease()
+                val routeCollector = RouteCollector(RouteDocumentationMerger())
 
+                // When
                 val actual = routeCollector.getPath(allRoutes[0], PluginConfigData.DEFAULT)
-                println("actual = $actual")
+
+                // Then
                 actual shouldBe "/v1/get_this"
             }
+        }
+    }
+}
+
+object ExternalCode {
+    private class TransparentRouteSelector : RouteSelector() {
+        override suspend fun evaluate(context: RoutingResolveContext, segmentIndex: Int): RouteSelectorEvaluation =
+            RouteSelectorEvaluation.Transparent
+
+        override fun toString(): String = ""
+    }
+
+    fun Route.installTransparentRoute(block: Route.() -> Unit) {
+        createChild(TransparentRouteSelector()).route {
+            block()
         }
     }
 }
